6c0b0c27f11041e677dd1393be8f8eeba0a3d135
Daniel Tavares <daniel.tavares@corp.badoo.com>
2018-04-11 11:43:06 +0100

    disable cloning of TheBB layers on setup, as I'm not using any of it

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-[[ -d ~/.spacemacs.d/layers/+TheBB ]] || \
-    git clone git@github.com:TheBB/spacemacs-layers.git ~/.spacemacs.d/layers/+TheBB
+#[[ -d ~/.spacemacs.d/layers/+TheBB ]] || \
+#    git clone git@github.com:TheBB/spacemacs-layers.git ~/.spacemacs.d/layers/+TheBB
 
 # Disable app nap for emacs so it doesn't cause terrible difficult to track down
 # things to happen https://github.com/syl20bnr/spacemacs/issues/5413
