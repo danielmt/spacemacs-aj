934458c65ca57831277f234d5ba5c9ea2b94d2c9
Daniel Tavares <daniel.tavares@corp.badoo.com>
2018-04-12 10:22:14 +0100

    fixing C-e with better-defaults, and removing unused commented bindings

--- lisp/init-evil.el
+++ lisp/init-evil.el
@@ -3,26 +3,19 @@
   (add-hook 'prog-mode-hook 'turn-on-evil-mc-mode)
   (add-hook 'text-mode-hook 'turn-on-evil-mc-mode)
 
-  ;; Move text up and down in visual mode with J/K
-  ;; (define-key evil-visual-state-map "J"
-  ;;   (concat ":move '>+1" (kbd "RET") "gv=gv"))
-  ;; (define-key evil-visual-state-map "K"
-  ;;   (concat ":move '<-2" (kbd "RET") "gv=gv"))
-
-  ;; make j & k behave as g j & g k:
-  ;; (define-key evil-normal-state-map (kbd "j") 'evil-next-visual-line)
-  ;; (define-key evil-normal-state-map (kbd "k") 'evil-previous-visual-line)
-
   ;; Use more simple */# so we don't drop into symbol-highlight mode
   ;; (define-key evil-normal-state-map (kbd "*") 'evil-search-word-forward)
   ;; (define-key evil-normal-state-map (kbd "#") 'evil-search-word-backward)
 
   ;; Remove C-a and C-b from ex (:) so we can use the regular emacs varieties
-  ;; (define-key evil-ex-completion-map (kbd "C-a") nil)
-  ;; (define-key evil-ex-completion-map (kbd "C-b") nil)
+  (define-key evil-ex-completion-map (kbd "C-a") nil)
+  (define-key evil-ex-completion-map (kbd "C-b") nil)
 
   ;; Remove this mapping so C-z can suspend in tty
-  ;; (define-key evil-motion-state-map (kbd "C-z") nil)
+  (define-key evil-motion-state-map (kbd "C-z") nil)
+
+  ;; Remove C-e so better-defaults take it
+  (define-key evil-motion-state-map (kbd "C-e") nil)
   )
 
 (defun minibuffer-inactive-mode-hook-setup ()
